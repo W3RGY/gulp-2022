277fc685d7e0ff16df343cc30d4370b8460cc6b2
Dima Osenkov <w3rgy1992@gmail.com>
2022-09-16 21:50:53 +0200

    3

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -2,10 +2,12 @@ import replace from "gulp-replace"; //search and replace
 import plumber from "gulp-plumber"; //error processing
 import notify from "gulp-notify"; //message (hint)
 import browsersync from "browser-sync" //local server
+import newer from "gulp-newer"; //check for updates
 
 export const plugins = {
 	replace: replace,
 	plumber: plumber,
 	notify: notify,
-	browsersync: browsersync
+	browsersync: browsersync,
+	newer: newer
 }
